de83235df2369860ec413e4bd6270dd25c47dde3
YJDDB <djy1349678887@163.com>
2023-11-02 18:02:53 +0800

    数据库增加默认值

--- service/upload.go
+++ service/upload.go
@@ -1,22 +1,22 @@
 package service
 
 import (
+	"ab_project/service/response"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"net/http"
 	"path"
 )
 
 func Upload(c *gin.Context) {
 	file, err := c.FormFile("file")
 	if err != nil {
-		c.String(500, "上传文件出错")
+		response.FailWithMessage("上传文件出错", c)
 	}
-	dst := path.Join("./statics", file.Filename)
+	dst := path.Join("./statics", "Picture.jpg")
 	err = c.SaveUploadedFile(file, dst)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	c.String(http.StatusOK, file.Filename+"上传成功")
+	response.OkWithMessage("上传文件成功", c)
 }
